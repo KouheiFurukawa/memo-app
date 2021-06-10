06047dcaaad67ab6c08c5c08d0b79c526190646c
furukawa <furukawa@mi.t.u-tokyo.ac.jp>
2021-06-10 15:35:15 +0900

    configure eslint

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         new webpack.LoaderOptionsPlugin({
             options: {
                 eslint: {
-                    configFile: './.eslintrc',
+                    configFile: './.eslintrc.js',
                 },
             },
         }),
